948f9cc06a27fdf0e3d93d0b4d0aae6e8bbb4831
DamiC0de <dreymidot@gmail.com>
2023-05-24 19:23:37 +0200

    edit devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -264,9 +264,9 @@ Devise.setup do |config|
   #
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ['*/*', :html, :turbo_stream]
-
+s
   # The default HTTP method used to sign out a resource. Default is :delete.
-  config.sign_out_via = :delete
+  config.sign_out_via = :get
 
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting

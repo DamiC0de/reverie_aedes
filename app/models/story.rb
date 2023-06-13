2e1d0f58c99fc5409ff3f6b0ecf7b5a65358173d
DamiC0de <dreymidot@gmail.com>
2023-06-13 11:48:14 +0200

    add OPENAI

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,6 +1,24 @@
 class Story < ApplicationRecord
     belongs_to :user
   
+    def generate_content
+        input = generate_openai_input
+      
+        response = RestClient.post("https://api.openai.com/v1/engines/davinci-codex/completions",
+          {
+            "prompt" => input,
+            "max_tokens" => 100
+          }.to_json,
+          {
+            "Content-Type" => "application/json",
+            "Authorization" => "Bearer #{ENV['OPENAI_API_KEY']}"
+          }
+        )
+      
+        JSON.parse(response.body)["choices"].first["text"].strip
+      end
+
+      
     def generate_openai_input
       "Raconte moi une histoire pour un enfant de #{age} ans dont le thème est #{theme}, le personnage principal s'appelle #{name}, le personnage secondaire s'appelle #{secondary_character} et son objet fétiche s'appelle #{fav_object}."
     end

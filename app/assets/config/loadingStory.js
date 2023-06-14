24b9e4ec40307221a4fb93754f5417df0483eb4c
DamiC0de <dreymidot@gmail.com>
2023-06-14 11:36:41 +0200

    add loading when history is generated

--- app/assets/config/loadingStory.js
+++ app/assets/config/loadingStory.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('form').on('submit', function() {
+      console.log("Form submitted"); // Ajouter cette ligne
       $('#loading-screen').show();
       $('body').css('pointer-events', 'none');
     });
   });
-  
--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link burgerNav.js
+//= require jquery3
 //= link loadingStory.js
 //= require rails-ujs
-//= require jquery3
